a9321b7b0e248f48b37dbf6ff18f702136c8a00a
AmaefuleVictor1 <125184094+AmaefuleVictor1@users.noreply.github.com>
2024-06-09 23:29:03 +0100

    Add files via upload

---
+++ personalwebsite/includes/navbar.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top py-2">
+    <div class="container px-1">
+        <a class="navbar-brand " href="index.php">
+        <img class="img-fluid " style="height: 45px;" src="./assets/logo-no-background.svg"><span class="text-gradient" style="font-size: 30px; font-weight:700;">| Amaefule Victor</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="resume.php">Resume</a></li>
+                <li class="nav-item"><a class="nav-link" href="projects.php">Projects</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
